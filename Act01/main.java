9b27a9c44d5d84c5719f182ab1afce58f5a60a81
Yeray <you@example.com>
2024-09-19 17:11:02 +0200

    first commit

---
+++ Act01/main.java
@@ -0,0 +1,4 @@
+/**
+ * El xifrat ROT13 consisteix a substituir cada lletra per la que està 13 posicions més a la dreta
+    alfabèticament, i si s’arriba al final de l’alfabet es torna a començar per el principi.
+ */
