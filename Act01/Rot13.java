f9f8f8d3fcf2590b732d9eb4c55be0d1ee683a96
Yeray <you@example.com>
2024-09-26 17:22:59 +0200

    Change main & use String Builder

--- Act01/Rot13.java
+++ Act01/Rot13.java
@@ -1,15 +1,15 @@
+
+
 /**
  * El xifrat ROT13 consisteix a substituir cada lletra per la que està 13 posicions més a la dreta
     alfabèticament, i si s’arriba al final de l’alfabet es torna a començar per el principi.
 */
 
-import java.util.Scanner;
-
 public class Rot13 {
 
     // Sugerencia: Les constants han d'anar amb majuscula per millor lectura
-    private static final char[] minuscules = {'a', 'à', 'b', 'c', 'ç', 'd', 'e', 'è', 'é', 'f', 'g', 'h', 'i', 'í', 'ï', 'j', 'k', 'l', 'm', 'n', 'o', 'ò', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z'};
-    private static final char[] majuscules = {'A', 'À', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Í', 'Ï', 'J', 'K', 'L', 'M', 'N', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'V', 'W', 'X', 'Y', 'Z'};
+    private static final char[] MINUSCULES = {'a', 'à', 'b', 'c', 'ç', 'd', 'e', 'è', 'é', 'f', 'g', 'h', 'i', 'í', 'ï', 'j', 'k', 'l', 'm', 'n', 'o', 'ò', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z'};
+    private static final char[] MAJUSCULES = {'A', 'À', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Í', 'Ï', 'J', 'K', 'L', 'M', 'N', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'V', 'W', 'X', 'Y', 'Z'};
 
     private static int indexOf(char[] cadena, char caracter){
         for(int i = 0; i < cadena.length; i++){
@@ -29,45 +29,38 @@ public class Rot13 {
     public static String desxifraRot13( char[] cadena ){
         return translate(cadena, false);
     }
-
-    public static String translate( char[] cadena, boolean encrypt){
-        String result = ""; // recomenació: al ser un string que es modifica tota l'estona utilitza sting buffed
-        for (int i = 0; i < cadena.length; i++){
-            if(Character.isLetter(cadena[i])){ //sugerencia: buscartots els mòduls aprovats i mitjana notes >= 8 dirctament en les arrays de lletres per fer el codi més optim ja que retornaria la posició
-                if(Character.isUpperCase(cadena[i])){
-                    if(encrypt){
-                        result += majuscules[(indexOf(majuscules, cadena[i]) + 13) % majuscules.length];
-                    }else{
-                        result += majuscules[(indexOf(majuscules, cadena[i]) - 13) % majuscules.length];
+    public static String translate( String text, boolean encrypt){ return translate(text.toCharArray(), encrypt);}
+    public static String translate(char[] cadena, boolean encrypt) {
+        StringBuilder result = new StringBuilder(); // recomenació: al ser un string que es modifica tota l'estona utilitza StringBuilder
+        for (int i = 0; i < cadena.length; i++) {
+            if (Character.isLetter(cadena[i])) { // sugerencia: buscartots els mòduls aprovats i mitjana notes >= 8 directament en les arrays de lletres per fer el codi més optim ja que retornaria la posició
+                if (Character.isUpperCase(cadena[i])) {
+                    if (encrypt) {
+                        result.append(MAJUSCULES[(indexOf(MAJUSCULES, cadena[i]) + 13) % MAJUSCULES.length]);
+                    } else {
+                        result.append(MAJUSCULES[(indexOf(MAJUSCULES, cadena[i]) - 13 + MAJUSCULES.length) % MAJUSCULES.length]);
                     }
                 } else {
-                    if(encrypt){
-                        result += minuscules[(indexOf(minuscules, cadena[i]) + 13) % minuscules.length];
-                    }else{
-                        result += minuscules[(indexOf(minuscules, cadena[i]) - 13) % minuscules.length];
+                    if (encrypt) {
+                        result.append(MINUSCULES[(indexOf(MINUSCULES, cadena[i]) + 13) % MINUSCULES.length]);
+                    } else {
+                        result.append(MINUSCULES[(indexOf(MINUSCULES, cadena[i]) - 13 + MINUSCULES.length) % MINUSCULES.length]);
                     }
                 }
             } else {
-                result += cadena[i];
+                result.append(cadena[i]);
             }
         }
-        return result;
+        return result.toString();
     }
 
     public static void main(String[] args){
-        if(args.length == 0){
-            System.out.println("ERROR: parametro no indicat.\n Usa $java Rot13 encrypt/desencrypt ");
-            return;
-        }
-        Scanner scanner = new Scanner(System.in);
-        if(args[0].equals("encrypt")){
-            System.out.println("Introdueix un text per encriptar:");
-            System.out.println(xifraRot13(scanner.nextLine()));
-            scanner.close();
-        }else if(args[0].equals("desencrypt")){
-            System.out.println("Introdueix un text per desencriptar:");
-            System.out.println(desxifraRot13(scanner.nextLine()));
-            scanner.close();
-        }
+        String fraseDeprova = "Hola que tal, sóc en Yeray!";
+        System.out.println("Aquest serà el text de prova que usarem: " + fraseDeprova);
+
+        String textTraduit = translate(fraseDeprova, true);
+        System.out.println("Codificat:   " + textTraduit);
+
+        System.out.println("Descodificat:   " + translate(textTraduit, false));
     }
 }
