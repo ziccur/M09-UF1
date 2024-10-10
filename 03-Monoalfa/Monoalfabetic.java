bcd1c74d860510b08aa20ee8ae0a1384570a8ae2
Yeray <you@example.com>
2024-10-10 18:09:06 +0200

    acabat

--- 03-Monoalfa/Monoalfabetic.java
+++ 03-Monoalfa/Monoalfabetic.java
@@ -6,9 +6,8 @@ public class Monoalfabetic{
 
     private static final char[] MINUSCULES = "aàábcçdeèéfghiìíïjklmnñoöòópqrstuùúüvwxyz".toCharArray(); 
     private static final char[] MAJUSCULES = "AÀÁBCÇDEÈÉFGHIÌÍÏJKLMNÑOÖÒÓPQRSTUÙÚÜVWXYZ".toCharArray();
-
-    private static final char[] MINUSCULESPERMUTAT = permutaAlfabet(MINUSCULES);
-    private static final char[] MAJUSCULESPERMUTAT = permutaAlfabet(MAJUSCULES);
+    private static final char[] MINUSCULESPERMUTADA = permutaAlfabet(MINUSCULES); 
+    private static final char[] MAJUSCULESPERMUTADA = new String(MINUSCULESPERMUTADA).toUpperCase().toCharArray();
 
     public static char[] permutaAlfabet(char[] alfabet){
         char[] result = new char[alfabet.length];
@@ -26,6 +25,57 @@ public class Monoalfabetic{
         return result;
     }
 
+
+   private static int indexOf(char[] var0, char var1) {
+        for(int var2 = 0; var2 < var0.length; ++var2) {
+            if (var0[var2] == var1) {
+                return var2;
+            }
+        }
+        return -1;
+    }
+
+    public static String xifraMonoAlfa(String cadena){
+        StringBuilder sb = new StringBuilder();
+        for(char c : cadena.toCharArray()){
+            if(Character.isLetter(c)){
+                if(Character.isUpperCase(c)){
+                    sb.append(MAJUSCULESPERMUTADA[indexOf(MAJUSCULES, c)]);
+                }else{
+                    sb.append(MINUSCULESPERMUTADA[indexOf(MINUSCULES, c)]);
+                }
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String desxifraMonoAlfa(String cadena){
+        StringBuilder sb = new StringBuilder();
+        for(char c : cadena.toCharArray()){
+            if(Character.isLetter(c)){
+                if(Character.isUpperCase(c)){
+                    sb.append(MAJUSCULES[indexOf(MAJUSCULESPERMUTADA, c)]);
+                }else{
+                    sb.append(MINUSCULES[indexOf(MINUSCULESPERMUTADA, c)]);
+                }
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("#########################\n#  Prova de xifratge    #\n#########################");
+
+        String[] textos = {"!¿Hola, com estàs?!", "12345abc67890", "    2a5    ", "aàábcçdeèéfghiìíïjklmnñoöòópqrstuùúüvwxyz" };
+
+        for(String text : textos){
+            System.out.println(text + " -> " + xifraMonoAlfa(text) + " -> " + desxifraMonoAlfa(xifraMonoAlfa(text)));
+        }
+    }
 
 
 
